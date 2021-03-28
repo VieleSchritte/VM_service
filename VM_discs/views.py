3adc0b382ce259a3001147eda0cfb4c2ddfb57c4
VieleSchritte <ohheavenineed@gmail.com>
2021-03-28 20:57:22 +0300

    after pull

--- VM_discs/views.py
+++ VM_discs/views.py
@@ -4,5 +4,5 @@ from .discs_manipulation import Discs
 
 def disks_table(request):
     d = Discs()
-    d.get_discs()
-    return render(request, 'disks_table.html', {})
+    clear_discs = d.get_discs()
+    return render(request, 'disks_table.html', {{clear_discs}})
