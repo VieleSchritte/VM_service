a272e218effde6d2b295b56b17186f0861adb40b
VieleSchritte <ohheavenineed@gmail.com>
2021-03-28 20:55:40 +0300

    function for getting discs done

--- VM_discs/discs_manipulation.py
+++ VM_discs/discs_manipulation.py
@@ -1,11 +1,21 @@
 from blkinfo import BlkDiskInfo
-import json
 
 
 class Discs:
     @staticmethod
     def get_discs():
         b = BlkDiskInfo()
-        all_disks = b.get_disks()
-        json_output = json.dumps(all_disks)
-        print(json_output)
+        all_discs = b.get_disks()
+        table_keys = ["name", "size", "mountpoint"]
+        clear_discs = []
+        for disc in all_discs:
+            temporal_dict = {}
+            for key in table_keys:
+                if key == 'mountpoint' and disc[key] == '':
+                    temporal_dict[key] = '-'
+                else:
+                    temporal_dict[key] = disc[key]
+            clear_discs.append(temporal_dict)
+        print('======', clear_discs)
+        del all_discs
+        return clear_discs
