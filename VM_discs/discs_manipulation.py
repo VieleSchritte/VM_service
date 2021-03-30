b8ea4f0a2ea64cec6c6e420ebd5ee69654593e0d
VieleSchritte <ohheavenineed@gmail.com>
2021-03-30 13:11:57 +0300

    new manipulation

--- VM_discs/discs_manipulation.py
+++ VM_discs/discs_manipulation.py
@@ -6,8 +6,9 @@ class Discs:
     @staticmethod
     def get_discs():
         all_discs = []
-        os.system('lsblk >> lsblk_out.txt')
-        with open ('lsblk_out.txt') as lsblk_out:
+        os.system('mkdir tmp')
+        os.system('lsblk >> tmp/lsblk_out.txt')
+        with open ('tmp/lsblk_out.txt') as lsblk_out:
             for line in lsblk_out:
                 line = line.strip().split()
                 if line[0] == "NAME":
@@ -15,5 +16,9 @@ class Discs:
                 temporal_dict = {"name": line[0], "size": line[3]}
                 if len(line) < 7:
                     temporal_dict["mountpoint"] = ''
-            all_discs.append(temporal_dict)
+                else:
+                    temporal_dict["mountpoint"] = line[6]
+                all_discs.append(temporal_dict)
+        os.system('rm -rf tmp')
+        print(all_discs)
         return all_discs
